87d8f644a71cd5542545d4eec32ecc0593ecf29e
rewonc <rewonc@gmail.com>
2014-11-17 22:35:09 -0800

    Add form

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
-  var _;
-  var friends, index, count;
-  var $quiz_container = ("<div class='quiz_container'></div>");
+  var friends, index, count, me;
+  var $quiz_container = $("<div class='quiz_container'></div>");
+
+  var renderFriend = function(){
+    if(friends.length > 0){
+      $quiz_container.html("<img src='https://graph.facebook.com/" + friends[0].id + "/picture?type=large', width='120', height='120'></img>");
+    } else {
+      $quiz_container.text("Hooray. You're done.");
+    }
+  };
 
   $.ajax("/api/facebook").done(function(data){
 
     //Select a random set of 20 ids
+    friends = _.shuffle(_.sample(data.friends, 20));
+    me = data.me;
 
-    friends = _.shuffle(_.sample(data, 20));
 
     console.log(friends);
 
-  	$(".row").after().append($quiz_container);
+    renderFriend();
+    $(".row").after().append($quiz_container);
+    
+    /*
   	var $banner = $("<div class='banner'></div>");
   	$(".quiz_container").append($banner);
 
   	var $whats_my_name = $("<div id='what'></div>");
   	$whats_my_name.html("WHAT IS MY NAME?");
-		$banner.append($whats_my_name).hide().fadeIn("4000");
+		$banner.append($whats_my_name).hide().fadeIn("4000"); */
 		// $(".banner").remove($whats_my_name);
 		//$whats_my_name.hide(); //.fadeOut();
 		// $whats_my_name.html("").html("WHAT IS");
@@ -30,14 +41,14 @@ $(document).ready(function() {
 		// $whats_my_name.html("WHAT IS MY NAME?");
 		// $(".banner").append($whats_my_name);
 		// $whats_my_name.remove();
-
+    /*
   	var count = 10;
   	friends = data.friends;
     
     while (count > 0) {
   	console.log(friends[count]["id"]);
   	count -= 1;
-  	}
+  	}*/
 
 
 
