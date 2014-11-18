62e0730acfe1e8cf784b73810f138e1faaaa6745
Matt Conrad <mmconrad@gmail.com>
2014-11-17 21:19:45 -0800

    Added quiz_container and text to begin quiz

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,42 @@
 $(document).ready(function() {
 
-  var friends;
+  var friends, index, count;
+  var $quiz_container = ("<div class='quiz_container'></div>");
 
   $.ajax("/api/facebook").done(function(data){
-    console.log(data);
-    friends = data.friends;
-    $('#content').html(JSON.stringify(friends));
+  	
+  	$(".row").after().append($quiz_container);
+  	var $banner = $("<div class='banner'></div>");
+  	$(".quiz_container").append($banner);
+
+  	var $whats_my_name = $("<div id='what'></div>");
+  	$whats_my_name.html("WHAT IS MY NAME?");
+		$banner.append($whats_my_name).hide().fadeIn("4000");
+		// $(".banner").remove($whats_my_name);
+		//$whats_my_name.hide(); //.fadeOut();
+		// $whats_my_name.html("").html("WHAT IS");
+		// $(".banner").append($whats_my_name).fadeIn();
+		// $whats_my_name.remove();
+
+		// $whats_my_name.html("WHAT IS MY");
+		// $(".banner").append($whats_my_name);
+		// $whats_my_name.remove();
+		// $whats_my_name.html("WHAT IS MY NAME?");
+		// $(".banner").append($whats_my_name);
+		// $whats_my_name.remove();
+
+  	var count = 10;
+  	friends = data.friends;
+    
+    while (count > 0) {
+
+  	console.log(friends[0]["id"]);
+  	count -= 1;
+  	}
+
+
+
+  //console.log(data);
+  //  $('#content').html(JSON.stringify(friends));
   });
-
-
-
 });
