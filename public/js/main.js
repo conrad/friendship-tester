6043d93cf58f8664af66ce4d34e047fec9404923
Matt Conrad <mmconrad@gmail.com>
2014-11-18 00:37:01 -0800

    adding success/failure messages; but not syncing with gameflow

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
 
-  var friends, index, count, me;
+  var friends, index, count, me, success_image;
   var $quiz_container = $("<div class='quiz_container'></div>");
   var $banner = $("<div class='banner'></div>");
   var $guess = $("#guess");
   var $whats_my_name = $("<div id='what'></div>");
+  // var $success = $("<div id='success'><img src='../img/success-1.png'></div>");
+  // ?var $success_image = $("<img>")
+  // $success.attr("src", "/img/success-1")
+  // var success_array = ["/img/success-1", "/img/success-2", "../img/success-3", "/img/success-4", "/img/success-5", "/img/success-6"];
+  $not_success = $("<div class='fail'></div>");
+  $not_success.text("That's not quite it")
 
   var renderFriend = function(){
     if(friends.length > 0){
-      $quiz_container.html("<img src='https://graph.facebook.com/" + friends[0].id + "/picture?type=large', width='200', height='200'></img>");
+      $quiz_container.html("<img src='https://graph.facebook.com/" + friends[0].id + "/picture?type=large', width='300', height='auto'></img>");
     } else {
       $quiz_container.text("Hooray. You're done.");
     }
@@ -17,17 +23,22 @@ $(document).ready(function() {
   var guess = function(){
     if( $guess.val() === friends[0].name){
       //maybe wait a second, display success, then next
-      friends.shift();
+      $(".quiz_container").prepend("<img src='../img/success-1.png', width='400', height='400' />");
+      console.log($success);
+
+      _.delay(friends.shift(), 5000);
       renderFriend();
     } else {
       //handle failure
+
+
     }
   }
 
   $(".row").after().append($quiz_container);
-  $(".quiz_container").append($banner);
+  $(".quiz_container").prepend($banner);
   $whats_my_name.html("WHAT'S MY NAME?");
-  $banner.append($whats_my_name).hide().fadeIn("6000");
+  $banner.prepend($whats_my_name); //.hide().fadeIn("6000");
 
   $("#guess").keyup(function(){
     guess();
@@ -53,7 +64,7 @@ $(document).ready(function() {
       // Fill an array of id's?
       id = friends[index]["id"];
       id_array.push(id);
-<<<<<<< HEAD
+
       
       // Add the photo
       $("#photo").html("<img src='user_photo[id]'>");    // what is the actual src that will work???
