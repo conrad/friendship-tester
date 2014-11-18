2f837b424fdf8f8da245d1ebd576522e25c4e6d4
Matt Conrad <mmconrad@gmail.com>
2014-11-17 22:29:58 -0800

    starting to build gameplay loop

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-  var friends, index, count;
+  var friends, index, count, photo;
+
   var $quiz_container = ("<div class='quiz_container'></div>");
 
   $.ajax("/api/facebook").done(function(data){
@@ -10,29 +11,28 @@ $(document).ready(function() {
   	$(".quiz_container").append($banner);
 
   	var $whats_my_name = $("<div id='what'></div>");
-  	$whats_my_name.html("WHAT IS MY NAME?");
+  	$whats_my_name.html("WHAT'S MY NAME?");
 		$banner.append($whats_my_name).hide().fadeIn("4000");
-		// $(".banner").remove($whats_my_name);
-		//$whats_my_name.hide(); //.fadeOut();
-		// $whats_my_name.html("").html("WHAT IS");
-		// $(".banner").append($whats_my_name).fadeIn();
-		// $whats_my_name.remove();
+		// $(".banner").remove($whats_my_name); $whats_my_name.hide(); //.fadeOut(); $whats_my_name.html("").html("WHAT IS"); $(".banner").append($whats_my_name).fadeIn(); $whats_my_name.remove(); $whats_my_name.html("WHAT IS MY"); $(".banner").append($whats_my_name); $whats_my_name.remove(); $whats_my_name.html("WHAT IS MY NAME?"); $(".banner").append($whats_my_name); $whats_my_name.remove();
 
-		// $whats_my_name.html("WHAT IS MY");
-		// $(".banner").append($whats_my_name);
-		// $whats_my_name.remove();
-		// $whats_my_name.html("WHAT IS MY NAME?");
-		// $(".banner").append($whats_my_name);
-		// $whats_my_name.remove();
-
-  	var count = 10;
-  	friends = data.friends;
+  	var count = 10,
+    id;
+  	var id_array = [];
+    friends = data.friends;
     
     while (count > 0) {
+      // Load random photo
+      index = Math.floor(Math.random() * friends.length)
+      // Fill an array of id's?
+      id = friends[index]["id"];
+      id_array.push(id);
+      // Or we could just load the photo here and have them guess
+      user_photo[id]
 
-  	console.log(friends[0]["id"]);
-  	count -= 1;
+      count -= 1;
   	}
+    // for (var i = 0; i < 10; i++) {
+    // }
 
 
 
